c714bf79a1e39248c2ab6ec5caca3d623e93a16c
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-03-28 16:19:57 -0500

    Terminando js.DMV

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const txtCharacter = document.getElementById("txt-character");
 const containerCards = document.getElementById("containerCards");
 const URL1 = "https://rickandmortyapi.com/api/character";
 const URL2 = "https://rickandmortyapi.com/api/character/?name="
+
+const getApi = async (URL) => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data.results;
+}
+
 const createCard = (character) => {
   const card = document.createElement("div");
   card.classList.add("card-character");
@@ -21,24 +28,17 @@ const createCard = (character) => {
   card.appendChild(imgCard);
   card.appendChild(containerDescription);
 
-
-
-
   containerCards.appendChild(card);
 };
 
 
-const getApi = async (URL) => {
-  const response = await fetch(URL);
-  const data = await response.json();
-  return data.results;
-}
+
 const generateAllCharacters = async () => {
   const data = await getApi(URL1);
   data.map(character => createCard(character));
 }
 
-const getCharacterByName = async () => {
+const getCharacterByName = async (event) => {
   containerCards.innerHTML = "";
   const data = await getApi(URL2 + event.target.value);
   data.map(character => createCard(character));
